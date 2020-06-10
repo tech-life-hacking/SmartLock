c425478c4e5700197700eb28682b377851615e4f
tech-life-hacking <63565392+tech-life-hacking@users.noreply.github.com>
2020-06-10 08:43:06 -0700

    modify testing program
    add face_location

--- plot_face_distance.py
+++ plot_face_distance.py
@@ -6,35 +6,41 @@ import seaborn as sns
 
 # Load some images to compare against
 picture = face_recognition.load_image_file("picture_of_me/0.jpg")
-encoding = face_recognition.face_encodings(picture)[0]
+face_location = face_recognition.face_locations(picture, number_of_times_to_upsample=0, model="cnn")
+encoding = face_recognition.face_encodings(picture,known_face_locations=face_location)[0]
 my_face_encodings = []
 unknown_face_encodings = []
+my_face_failure_count = 0
+unknown_face_failure_count = 0
 
 print("load images")
 for i in range(100):
     picture_of_me = face_recognition.load_image_file("picture_of_me/"+str(i)+".jpg")
     unknown_picture = face_recognition.load_image_file("unknown_picture/"+str(i)+".jpg")
     try:
-        my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
+        my_face_location = face_recognition.face_locations(picture_of_me, number_of_times_to_upsample=0, model="cnn")
+        my_face_encoding = face_recognition.face_encodings(picture_of_me,known_face_locations=my_face_location)[0]
         my_face_encodings.append(my_face_encoding)
     except:
-        pass
+        my_face_failure_count += 1
     try:
-        unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
+        unknown_location = face_recognition.face_locations(unknown_picture, number_of_times_to_upsample=0, model="cnn")
+        unknown_face_encoding = face_recognition.face_encodings(unknown_picture,known_face_locations=unknown_location)[0]
         unknown_face_encodings.append(unknown_face_encoding)
     except:
-        pass
+        unknown_face_failure_count += 1
     if i == 50:
         print("Completes to load 50 images")
         print("Please wait to load the rest of 50 images")
 
 print("loading completes")
+print(my_face_failure_count,unknown_face_failure_count)
 
 my_face_distances = face_recognition.face_distance(my_face_encodings,encoding)
 unknown_face_distances = face_recognition.face_distance(unknown_face_encodings,encoding)
 
-my_face_distances = my_face_distances[0:90]
-unknown_face_distances = unknown_face_distances[0:90]
+my_face_distances = my_face_distances[0:100]
+unknown_face_distances = unknown_face_distances[0:100]
 
 sns.set()
 sns.set_style('whitegrid')
